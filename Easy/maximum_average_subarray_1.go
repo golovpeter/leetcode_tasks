d6fef8f4770942beb9ad96e15bf229fb76219c7f
Golov Petr <petya-golov@mail.ru>
2023-07-17 13:23:53 +0300

    Maximum Average Subarray I

---
+++ Easy/maximum_average_subarray_1.go
@@ -0,0 +1,29 @@
+package Easy
+
+func findMaxAverage(nums []int, k int) float64 {
+	var prevSum int
+	left, right := 0, k-1
+
+	for i := left; i <= right; i++ {
+		prevSum += nums[i]
+	}
+
+	maxAverage := float64(prevSum) / float64(k)
+	right++
+	left++
+
+	for right <= len(nums)-1 {
+		prevSum -= nums[left-1]
+		prevSum += nums[right]
+
+		curAverage := float64(prevSum) / float64(k)
+		if curAverage > maxAverage {
+			maxAverage = curAverage
+		}
+
+		left++
+		right++
+	}
+
+	return maxAverage
+}
