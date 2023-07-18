86ddeee39ebf65524c88798f34663ee31132c7d4
Golov Petr <petya-golov@mail.ru>
2023-07-18 16:16:36 +0300

    Combination sum

---
+++ Medium/combination_sum.go
@@ -0,0 +1,34 @@
+package Medium
+
+func combinationSum(candidates []int, target int) [][]int {
+	var res [][]int
+	var backtrack func(currComb []int, curSum int)
+
+	backtrack = func(currComb []int, curSum int) {
+		if curSum > target {
+			return
+		}
+
+		if curSum == target {
+			copyCurrComb := make([]int, len(currComb))
+			copy(copyCurrComb, currComb)
+			res = append(res, copyCurrComb)
+			return
+		}
+
+		for _, el := range candidates {
+			if el > target || (len(currComb) != 0 && el < currComb[len(currComb)-1]) {
+				continue
+			}
+
+			currComb = append(currComb, el)
+			backtrack(currComb, curSum+el)
+			currComb = currComb[:len(currComb)-1]
+		}
+
+		return
+	}
+
+	backtrack([]int{}, 0)
+	return res
+}
