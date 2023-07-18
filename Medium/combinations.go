4deccc161a975f856fe9f68c7b7f1a2c4a730b42
Golov Petr <petya-golov@mail.ru>
2023-07-18 14:35:02 +0300

    Combinations

---
+++ Medium/combinations.go
@@ -0,0 +1,26 @@
+package Medium
+
+func combine(n int, k int) [][]int {
+	var res [][]int
+	var backtrack func(start int, currComb []int)
+
+	backtrack = func(start int, currComb []int) {
+		if len(currComb) == k {
+			dst := make([]int, len(currComb))
+			copy(dst, currComb)
+			res = append(res, dst)
+			return
+		}
+
+		for i := start; i <= n; i++ {
+			currComb = append(currComb, i)
+			backtrack(i+1, currComb)
+			currComb = currComb[:len(currComb)-1]
+		}
+
+		return
+	}
+
+	backtrack(1, []int{})
+	return res
+}
