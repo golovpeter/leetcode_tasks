c68fb3ecc4625d4a4802138da6050e40a6519fc5
Golov Petr <petya-golov@mail.ru>
2023-07-19 15:43:58 +0300

    Longest Increasing Subsequence

---
+++ Medium/longest_increasing_subsequence.go
@@ -0,0 +1,24 @@
+package Medium
+
+func lengthOfLIS(nums []int) int {
+	dp := make([]int, len(nums))
+	result := 0
+
+	for i := 0; i < len(nums); i++ {
+		maxSub := 0
+
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] && dp[j] > maxSub {
+				maxSub = dp[j]
+			}
+		}
+
+		dp[i] = maxSub + 1
+
+		if dp[i] > result {
+			result = dp[i]
+		}
+	}
+
+	return result
+}
