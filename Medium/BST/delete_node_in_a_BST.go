b2f5ec69e2bb0aafdd7978ac796ea865fe6b16e9
Golov Petr <petya-golov@mail.ru>
2024-02-25 14:34:29 +0300

    Delete Node in a BST

---
+++ Medium/BST/delete_node_in_a_BST.go
@@ -0,0 +1,37 @@
+package BST
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	if key > root.Val {
+		root.Right = deleteNode(root.Right, key)
+	} else if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	} else {
+		if root.Right == nil {
+			return root.Left
+		}
+
+		if root.Left == nil {
+			return root.Right
+		}
+
+		cur := root.Right
+		for cur.Left != nil {
+			cur = cur.Left
+		}
+
+		root.Val = cur.Val
+		root.Right = deleteNode(root.Right, root.Val)
+	}
+
+	return root
+}
